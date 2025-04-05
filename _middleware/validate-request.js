c2b2333dd629eb7f76f3d768e640729b9ef039ee
NJLforthewin <johnchristiansancija0@gmail.com>
2025-04-05 20:26:37 +0800

    fixed and added working api's

--- _middleware/validate-request.js
+++ _middleware/validate-request.js
@@ -1,13 +1,13 @@
 module.exports = validateRequest;
 
 function validateRequest(req, next, schema) {
-    const option = {
-        abortEarly: false, // include all errors
-        allUnknown: true, // ignore unknown props
-        stripUnknown: true // remover unknown props
+    const options = { 
+        abortEarly: false, 
+        allowUnknown: true, 
+        stripUnknown: true
     };
-    const { error, value} = schema.validate(req, body, options);
-    if(error) {
+    const { error, value } = schema.validate(req.body, options); 
+    if (error) {
         next(`Validation error: ${error.details.map(x => x.message).join(', ')}`);
     } else {
         req.body = value;
