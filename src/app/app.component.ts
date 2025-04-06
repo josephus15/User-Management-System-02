971bc0018f878d54a39c6236e1134a2afc2704f2
NJLforthewin <johnchristiansancija0@gmail.com>
2025-04-07 02:28:53 +0800

    Add Angular cache to gitignore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,24 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { AccountService } from './_services';
-import { User, Role } from './_models'; // Change Account to User
+import { User, Role } from './_models';
 import { AlertComponent } from './components/alert.component';
 
 @Component({
-  selector: 'app',
+  selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
   imports: [
-    CommonModule,
-    RouterModule,
-    AlertComponent
+    CommonModule,  // For ngClass and other directives
+    RouterModule,  // For router-outlet
+    AlertComponent  // For <alert> component
   ]
 })
 export class AppComponent {
   Role = Role;
-  user?: User | null; // Change account to user and adjust type
+  user?: User | null;
 
   constructor(private accountService: AccountService) {
     this.accountService.user.subscribe(x => this.user = x);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-// src/app/app.module.ts
 import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
-// used to create fake backend
-import { fakeBackendProvider } from './_helpers';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { JwtInterceptor, ErrorInterceptor, appInitializer } from './_helpers';
 import { AccountService } from './_services';
 import { AppComponent } from './app.component';
-import { AlertComponent } from './components/alert.component';
-import { HomeComponent } from './home';
+import { fakeBackendProvider } from './_helpers';
 
 @NgModule({
     imports: [
@@ -20,18 +17,18 @@ import { HomeComponent } from './home';
         ReactiveFormsModule,
         HttpClientModule,
         AppRoutingModule,
-        // Import standalone components
-        AppComponent,
-        AlertComponent, 
-        HomeComponent
+        CommonModule,
+        RouterModule,
+        AppComponent  // MOVED TO IMPORTS INSTEAD OF DECLARATIONS
     ],
-    // No declarations since all components are standalone
+    declarations: [
+        // AppComponent REMOVED FROM HERE
+    ],
     providers: [
         { provide: APP_INITIALIZER, useFactory: appInitializer, multi: true, deps: [AccountService] },
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-        // provider used to create fake backend
-      //  fakeBackendProvider
+        fakeBackendProvider
     ],
     bootstrap: [AppComponent]
 })
