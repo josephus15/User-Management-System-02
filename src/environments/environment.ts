7ec50ad72a1841767dc0373684b81b79e16ce196
josephus15 <josephuserl15@gmail.com>
2025-05-14 15:42:50 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:4000'
+    production: true,
+    apiUrl: 'https://user-management-system-02.onrender.com' 
   };
-  
